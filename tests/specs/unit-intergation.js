59db5e9028ee68d388b10e029ae7c97304b9f761
sinoservices <sinoservices@sinoservicesdeMacBook-Pro.local>
2018-11-07 15:05:19 +0800

    all

--- tests/specs/unit-intergation.js
+++ tests/specs/unit-intergation.js
@@ -1,118 +1,109 @@
-import React from 'react';
-import {
-  render,
-  cleanup,
-  fireEvent,
-} from 'react-testing-library'
-import 'jest-dom/extend-expect'
-import detectPassiveEvents from 'detect-passive-events';
-import ScrollUpButton from '../../src/react-scroll-up-button';
+import React from "react";
+import { render, cleanup, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import detectPassiveEvents from "detect-passive-events";
+import ScrollUpButton from "../../src/react-scrollup";
 
-jest.mock('detect-passive-events')
+jest.mock("detect-passive-events");
 
 afterEach(() => {
-  cleanup()
-})
+  cleanup();
+});
 beforeEach(() => {
-  window.pageYOffset = 0
+  window.pageYOffset = 0;
   global.scrollTo = jest.fn((x, y) => {
-    window.pageXOffset = x
-    window.pageYOffset = y
-    window.dispatchEvent(new window.UIEvent('scroll'));
-  })
-})
+    window.pageXOffset = x;
+    window.pageYOffset = y;
+    window.dispatchEvent(new window.UIEvent("scroll"));
+  });
+});
 expect.extend({
   toBeWithinRange(received, floor, ceiling) {
     const pass = received >= floor && received <= ceiling;
     if (pass) {
       return {
-        message: () => `expected ${received} not to be within range ${floor} - ${ceiling}`,
-        pass: true,
+        message: () =>
+          `expected ${received} not to be within range ${floor} - ${ceiling}`,
+        pass: true
       };
     }
     return {
-      message: () => `expected ${received} to be within range ${floor} - ${ceiling}`,
-      pass: false,
+      message: () =>
+        `expected ${received} to be within range ${floor} - ${ceiling}`,
+      pass: false
     };
-  },
+  }
 });
 
-describe('React unit/intergration testing', () => {
-  test('Aside to be rendered', async () => {
-    const {
-      container,
-      getByTestId,
-    } = render(<ScrollUpButton />)
-    const Aside = getByTestId('react-scroll-up-button')
-    expect(container).toContainElement(Aside)
+describe("React unit/intergration testing", () => {
+  test("Aside to be rendered", async () => {
+    const { container, getByTestId } = render(<ScrollUpButton />);
+    const Aside = getByTestId("react-scroll-up-button");
+    expect(container).toContainElement(Aside);
   });
 
-  test('mock detect-passive-events, branch testing', async () => {
-    const {
-      rerender,
-    } = render(<ScrollUpButton />)
-    detectPassiveEvents.hasSupport = true
-    rerender(<ScrollUpButton />)
+  test("mock detect-passive-events, branch testing", async () => {
+    const { rerender } = render(<ScrollUpButton />);
+    detectPassiveEvents.hasSupport = true;
+    rerender(<ScrollUpButton />);
   });
 
-  test('transition class applied on scroll', async () => {
-    const {
-      getByTestId,
-    } = render(<ScrollUpButton />)
-    const Aside = getByTestId('react-scroll-up-button')
+  test("transition class applied on scroll", async () => {
+    const { getByTestId } = render(<ScrollUpButton />);
+    const Aside = getByTestId("react-scroll-up-button");
 
-    expect(Aside).toHaveClass('ScrollUpButton__Container')
-    window.pageYOffset = 160
-    window.dispatchEvent(new window.UIEvent('scroll'));
+    expect(Aside).toHaveClass("ScrollUpButton__Container");
+    window.pageYOffset = 160;
+    window.dispatchEvent(new window.UIEvent("scroll"));
 
-    expect(Aside).toHaveClass('ScrollUpButton__Toggled')
+    expect(Aside).toHaveClass("ScrollUpButton__Toggled");
   });
 
-  test('loses transition class after scrolling up', async (done) => {
-    const {
-      getByTestId,
-    } = render(<ScrollUpButton AnimationDuration={10} />)
-    const Aside = getByTestId('react-scroll-up-button')
-    expect(Aside).not.toHaveClass('ScrollUpButton__Toggled')
+  test("loses transition class after scrolling up", async done => {
+    const { getByTestId } = render(<ScrollUpButton AnimationDuration={10} />);
+    const Aside = getByTestId("react-scroll-up-button");
+    expect(Aside).not.toHaveClass("ScrollUpButton__Toggled");
 
-    window.pageYOffset = 160
-    window.dispatchEvent(new window.UIEvent('scroll'));
+    window.pageYOffset = 160;
+    window.dispatchEvent(new window.UIEvent("scroll"));
 
-    expect(Aside).toHaveClass('ScrollUpButton__Toggled')
-    fireEvent.click(Aside)
+    expect(Aside).toHaveClass("ScrollUpButton__Toggled");
+    fireEvent.click(Aside);
     setTimeout(() => {
-      expect(Aside).not.toHaveClass('ScrollUpButton__Toggled')
+      expect(Aside).not.toHaveClass("ScrollUpButton__Toggled");
       done();
-    }, 1000)
+    }, 1000);
   });
 
-  test('stopPosition props stops scrolling at desired position', async (done) => {
-    const {
-      getByTestId,
-    } = render(<ScrollUpButton AnimationDuration={10} StopPosition={30} />)
-    const Aside = getByTestId('react-scroll-up-button')
-    window.pageYOffset = 300
-    window.dispatchEvent(new window.UIEvent('scroll'));
+  test("stopPosition props stops scrolling at desired position", async done => {
+    const { getByTestId } = render(
+      <ScrollUpButton AnimationDuration={10} StopPosition={30} />
+    );
+    const Aside = getByTestId("react-scroll-up-button");
+    window.pageYOffset = 300;
+    window.dispatchEvent(new window.UIEvent("scroll"));
 
-    expect(Aside).toHaveClass('ScrollUpButton__Toggled')
-    fireEvent.click(Aside)
+    expect(Aside).toHaveClass("ScrollUpButton__Toggled");
+    fireEvent.click(Aside);
     setTimeout(() => {
-      expect(Aside).not.toHaveClass('ScrollUpButton__Toggled')
-      expect(global.scrollTo).toHaveBeenLastCalledWith(0, 30)
+      expect(Aside).not.toHaveClass("ScrollUpButton__Toggled");
+      expect(global.scrollTo).toHaveBeenLastCalledWith(0, 30);
       done();
-    }, 1000)
+    }, 1000);
   });
 
-  test('can accept props', async () => {
-    render(<ScrollUpButton
-      ShowAtPosition={200}
-      StopPosition={100}
-      style={{}}
-      ToggledStyle={{}}
-      ContainerClassName="NewClass"
-      TransitionClassName="AnotherClass"
-      EasingType="easeInOutBounce"
-      AnimationDuration={1000}
-    />)
+  test("can accept props", async () => {
+    render(
+      <ScrollUpButton
+        ShowAtPosition={200}
+        StopPosition={100}
+        style={{}}
+        ToggledStyle={{}}
+        ContainerClassName="NewClass"
+        TransitionClassName="AnotherClass"
+        EasingType="easeInOutBounce"
+        AnimationDuration={1000}
+      />
+    );
   });
 });
